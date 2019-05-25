9211cf555aaa6f80141012175f9111143ab37419
Kunoh <Kunoh@DESKTOP-2O8Q2AA.eduroam.wireless.dtu.dk>
2019-05-25 11:11:27 +0200

    1321312

--- src/com/beliefrevision/Main.java
+++ src/com/beliefrevision/Main.java
@@ -1,8 +1,50 @@
 package com.beliefrevision;
 
+import java.util.Scanner;
+
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.ExistingAnnotationsType;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	menu();
+    }
+    
+    public static void menu() {
+    	@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+   	
+    	for(;;) {
+	    	System.out.println("1. Add to belief base");
+	    	System.out.println("2. Check formula for consistency");
+	    	System.out.println("3. Show belief base");
+	    	System.out.println("4. Quit");
+	    	
+	    	//Read input for choice
+	    	int choice = in.nextInt();
+	    	in.nextLine();	
+	    	
+	    	switch (choice) {
+			case 1:
+				
+				break;
+				
+			case 2:
+				
+				break;
+				
+			case 3:
+				
+				break;
+	
+			case 4:
+				System.out.println("Belief engine shutdown");
+				System.exit(0);
+				break;
+	
+			default:
+				break;
+			}
+    	}
     }
 }
