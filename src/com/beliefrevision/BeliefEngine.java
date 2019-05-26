f871b7c719966cddb6ad52ff89a9169f2a39c21a
kunoh <kunoh@live.dk>
2019-05-26 10:31:16 +0200

    added print beliefBase (#4)

--- src/com/beliefrevision/BeliefEngine.java
+++ src/com/beliefrevision/BeliefEngine.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 
 public class BeliefEngine {
     ArrayList<Object> beliefBase = new ArrayList<>();
@@ -20,10 +22,28 @@ public class BeliefEngine {
         }
 
     }
+    
+    public void printBeliefBase() {
+    	int beliefBaseLength = beliefBase.size();
+    	int printCounter = 1;
+    	System.out.print("{");
+    	for(Object obj: beliefBase) {
+    		if(obj.getClass() == CNFSentence.class) {
+    			cnf = (CNFSentence) obj;
+    			System.out.print(cnf.getCNFSentence());
+    		}
+    		if(beliefBaseLength != printCounter) {
+    			System.out.print(", ");
+    			printCounter++;
+    		}
+    	}
+    	System.out.println("}");
+    }
 
     private boolean isCNF(String sentence) {
         Pattern pattern = Pattern.compile("([!]?[a-zA-Z](\\s?[|]\\s?([!]?[a-zA-Z]))+)|(([!]?[a-zA-Z]\\s?)|([(]([!]?[a-zA-Z](\\s?[|]\\s?[!]?[a-zA-Z])+)[)]\\s?))([&]\\s?(([!]?[a-zA-Z]\\s?)|([(]([!]?[a-zA-Z](\\s?[|]\\s?[!]?[a-zA-Z])+)[)]\\s?)))*");
         Matcher matcher = pattern.matcher(sentence);
         return matcher.matches();
     }
+    
 }
